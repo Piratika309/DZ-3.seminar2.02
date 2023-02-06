e928a9a94f38b5653dcdc768c8f643e888a58786
Natali <piratika309@mail.ru>
2023-02-07 02:25:21 +0300

    Сумма цифр

---
+++ example002/Program.cs
@@ -0,0 +1,12 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.WriteLine("Введите число: ");
+
+int Number = int.Parse(Console.ReadLine());
+int sum = 0;
+
+for (int i=Number; i>0; i= i/10)
+{
+    sum = sum + i % 10;
+}
+Console.WriteLine("Сумма цифр в числе равна : " +sum);
