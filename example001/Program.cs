c3418dee620c443e6853b5d0a2421ccb7631e687
Natali <piratika309@mail.ru>
2023-02-07 01:47:10 +0300

    Кубы чисел

---
+++ example001/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти кубы чисел от 1 до N
+
+Console.WriteLine(" Введите число, до которого хотите получить расчет кубов чисел : ");
+int N = int.Parse(Console.ReadLine());
+
+for (int i=1; i<=N; i++)
+{
+    Console.WriteLine(i+"\t"+i*i*i);
+}
