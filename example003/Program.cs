af5dd3b642cab1ff2d69ea087aa88abc3b7a16ee
Natali <piratika309@mail.ru>
2023-02-08 18:54:48 +0300

    Произведение чисел

---
+++ example003/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.WriteLine("Введите число до которого будет вычисляться произведения чисел: ");
+
+int N = int.Parse(Console.ReadLine());
+int result = 1;
+
+for (int i=1; i<=N; i++) result = result*i;
+ {
+    Console.Write("Произведение чисел равно: " + result);
+ }
