418f1d88d2cf5f16e02ffe2204226acc66285a9e
Natali <piratika309@mail.ru>
2023-02-09 13:55:08 +0300

    Четная цифра кубов

---
+++ example004/Program.cs
@@ -0,0 +1,21 @@
+﻿// Показать кубы чисел, заканчивающиеся на четную цифру
+
+int [] array = new int[7];
+
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0,20);
+}
+for (int i = 0; i < array.Length; i++)
+Console.WriteLine ($" {array[i]} \t");
+
+for (int i = 0; i < array.Length; i++)
+{
+    int number = array[i];
+    if ((array[i] % 2 == 0))
+    {
+        double cube = Math.Pow(array[i],3);
+        Console.WriteLine("Куб числа " + number + " равен " + cube + " и заканчивается на четную цифру");
+}
+}
